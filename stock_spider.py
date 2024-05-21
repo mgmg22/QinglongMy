47551569954f324bfb6181dac154d291c13c5510
mgmg22 <d33hc@qq.com>
2024-05-21 22:01:28 +0800

    feat:个股改为雪球数据

--- stock_spider.py
+++ stock_spider.py
@@ -5,16 +5,6 @@ new Env('上证指数');
 import sendNotify
 import requests
 import pysnowball as ball
-from stock_detail import get_stock_detail
-
-indexFilters = {
-    '上证指数',
-    # '上证50',
-    '科创50',
-    '创业板指',
-    # '沪深300',
-    '北证50',
-}
 
 fundFilters = {
     '酒ETF',
@@ -23,33 +13,18 @@ fundFilters = {
 }
 
 stockFilters = {
-    '600519',
-    '603605',
-    '603444',
+    'SH600519',
+    'SH603605',
+    'SH603444',
 }
 
 notifyData = []
-stockData = []
-
-
-# 指数涨幅统计
-def get_index_increase():
-    list_url = 'https://www.jisilu.cn/data/idx_performance/list/?___jsl=LST___t=1710685332681'
-    resp = requests.post(list_url)
-    for row in resp.json()['rows']:
-        if row['cell']['index_nm'] in indexFilters:
-            stock = {
-                'id': row['id'],
-                'name': row['cell']['index_nm'],
-                'increase': row['cell']['increase_rt'],
-            }
-            notifyData.append(stock)
 
 
 # 个股涨幅统计
 def get_stock_increase():
     for item in stockFilters:
-        stockData.append(get_stock_detail(item))
+        add_xq_increase(item)
 
 
 # etf涨幅统计
@@ -62,17 +37,23 @@ def get_fund_increase():
                 'id': row['id'],
                 'name': row['cell']['fund_nm'],
                 'increase': row['cell']['increase_rt'],
+                'current': row['cell']['fund_nav'],
+                'avg_price': '',
             }
             notifyData.append(stock)
 
 
 def add_xq_increase(code):
     quote = ball.quote_detail(code)['data']['quote']
+    # print(quote)
     notifyData.append({
         'id': quote['code'],
         'name': quote['name'],
-        'increase': quote['percent'],
+        'increase': str(quote['percent']),
+        'current': str(quote['current']),
+        'avg_price': str(quote['avg_price']),
     })
+    print(quote['name'] + str(quote['current']))
 
 
 #  专门获取医药生物（801150.SL）的数据
@@ -86,27 +67,22 @@ def add_sw_increase():
     data = resp.json()['data'][0]
     last_day = float(data['l3'])
     now = float(data['l8'])
-    swData = {
+    notifyData.append({
         'id': '801150',
         'name': "医药生物",
         'increase': str(round(((now - last_day) / last_day) * 100, 2)),
-    }
-    notifyData.append(swData)
+        'current': str(now),
+        'avg_price': '',
+    })
 
 
 def notify_with_markdown():
-    markdown_text = '''# 今日涨幅统计
-| 代码 | 名称 | 涨幅 |
-|--------|--------|--------|
-'''
-    for item in notifyData:
-        markdown_text += f'| {item["id"]} | {item["name"]} | {item["increase"]} %|\n'
-    markdown_text += '''# 今日个股涨幅统计
-| 代码 | 名称 | 现价 | 涨幅 |
+    markdown_text = '''# 今日行情
+| 名称 | 现价 | 涨幅 | 均价 |
 |--------|:--------|--------:|--------|
 '''
-    for item in stockData:
-        markdown_text += f'| {item["id"]} | {item["name"]} | {item["price"]} | {item["increase"]} |\n'
+    for item in notifyData:
+        markdown_text += f'| {item["name"]} | {item["current"]} | {item["increase"]} %| {item["avg_price"]} \n'
     sendNotify.serverJMy(generate_title(), markdown_text)
     with open("log_stock.md", 'w', encoding='utf-8') as f:
         f.write(markdown_text)
@@ -118,10 +94,10 @@ def generate_title() -> str:
 
 if __name__ == '__main__':
     ball.set_token('xq_a_token=e8bf59070a162a60f06134803dd747557a3dbc2e')
-    get_index_increase()
-    get_stock_increase()
-    get_fund_increase()
-    add_xq_increase('CSI930599')
+    add_xq_increase('SH000001')
     add_xq_increase('SZ399808')
     add_sw_increase()
+    add_xq_increase('CSI930599')
+    get_stock_increase()
+    get_fund_increase()
     notify_with_markdown()
