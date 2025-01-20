65a2bd3e3d349f6ca4e61ba97298d3b814c7e981
mgmg22 <d33hcx@qq.com>
2025-01-20 16:02:43 +0800

    fix:requests

--- wxpush_sendNotify.py
+++ wxpush_sendNotify.py
@@ -17,7 +17,7 @@ def send_wxpusher_html_message(summary: str, content: str, topic_id: str = None,
         "verifyPayType": verify  # 1:只发送给付费的用户，2:只发送给未订阅或者订阅过期的用户. Defaults to 0.
     }
     if topic_id:
-        payload["topicIds"] = topic_id
+        payload["topicIds"] = [int(topic_id)]
     if uids:
         payload["uids"] = [uids]
     if url:
@@ -29,9 +29,7 @@ def send_wxpusher_html_message(summary: str, content: str, topic_id: str = None,
         response = requests.post(WX_URL, headers=headers, data=json.dumps(payload))
         response.raise_for_status()
         json_response = response.json()
-        if json_response.get("success"):
-            return f"wxPush成功"
-        else:
-            return f"wxPush失败: {json_response.get('data')}"
+        # print(json_response)
+        return f"wxPush{json_response.get('msg')}"
     except requests.exceptions.RequestException as e:
         return f"wxPush请求错误: {e}"
